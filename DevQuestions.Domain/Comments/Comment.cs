777fa22f87f19bdbc2945ae9c2eb535391076647
Dima Karpenko dimak <dimka2010@gmail.com>
2025-10-25 17:18:24 +0300

    done with domain models

---
+++ DevQuestions.Domain/Comments/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace DevQuestions.Domain.Comments;
+
+public class Comment
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Comment? Parent { get; set; }
+    public List<Comment> Children { get; set; } = [];
+    public Guid EntityId { get; set; } // к чему оставили комментарий 
+    public string EntityType { get; set; } // под какой сущностью оставли комментарий
+}
