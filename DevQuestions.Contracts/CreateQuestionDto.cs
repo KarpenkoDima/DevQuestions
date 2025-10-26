2eda71479d5a51568ebf63e01b97eed9e5c99783
Dima Karpenko dimak <dimka2010@gmail.com>
2025-10-26 14:27:04 +0200

    Writen scaffolding for QuestionsController.cs

---
+++ DevQuestions.Contracts/CreateQuestionDto.cs
@@ -0,0 +1,6 @@
+﻿namespace DevQuestions.Presenters;
+
+public record CreateQuestionDto(string Title,  string Body, Guid UserId, Guid[] TagsIds);
+public record GetQuestionDto(string Search,  Guid[] TagsIds, int Page, int PageSize);
+public record UpdateQuestionDto(string Title, string Body, Guid UserId, Guid[] TagsIds);
+public record AddAnswerDto(Guid UserId, string text);
---
+++ DevQuestions.Presenters/QuestionController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevQuestions.Presenters;
+
+[ApiController]
+[Route("[controller]")]
+public class QuestionController : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<string>> Create([FromBody] CreateQuestionDto questionDto, CancellationToken cancellation)
+    {
+        return Ok("Question");
+    }
+
+    [HttpPost("{question_id}/answer")]
+    public async Task<IActionResult> AddAnswer([FromBody] AddAnswerDto questionDto, CancellationToken cancellation)
+    {
+        return Ok("Add answer");
+    }
+
+    // GET: /questions?tag_id=1&limit=1&title="test"
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] GetQuestionDto questionDto, CancellationToken cancellation)
+    {
+        return Ok("GetQuestion");
+    }
+
+    // GET: /quiestions/{question_id}
+    [HttpGet("{questionId:guid}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid questionId, CancellationToken cancellation)
+    {
+        return Ok("GetQuestion");
+    }
+
+    // PUT: /quiestions/{question_id}
+    [HttpPut("{questionId:guid}")]
+    public async Task<IActionResult> Update([FromRoute] Guid questionId, [FromBody]UpdateQuestionDto questionDto, CancellationToken cancellation)
+    {
+        return Ok("Update Question");
+    }
+
+    // DELETE: /quiestions/{question_id}
+    [HttpDelete("{questionId:guid}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid questionId, CancellationToken cancellation)
+    {
+        return Ok("Delete Question");
+    }
+
+    // PUT: /quiestions/{question_id}/solution
+    [HttpPut("{questionId:guid}/solution")]
+    public async Task<IActionResult> SelectSolution([FromRoute] Guid questionId, [FromQuery] Guid answerId, CancellationToken cancellation)
+    {
+        return Ok("Select solution");
+    }
+}
+
